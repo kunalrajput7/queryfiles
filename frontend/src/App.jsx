ac71cd4bb68f62968c9d3a53aae3fc983fe81eda
Kunal Rajput <kunalrajput15112000@gmail.com>
2025-05-01 03:54:51 -0500

    redeploy with casesensitive

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import { onAuthStateChanged } from "firebase/auth";
 import { auth } from "./firebaseConfig";
-import AuthPage from "./authentication/AuthPage";
+import AuthPage from "/src/authentication/AuthPage";
 import MainPage from "./pages/MainPage";
 
 function App() {
